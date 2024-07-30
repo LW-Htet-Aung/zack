dbdb5baed97dfcd0c624d30595230cf58638e5a8
LW-Htet-Aung <kiritokirikaya80@gmail.com>
2024-07-31 00:12:22 +0630

    Zack - fix some ui bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { createUser } from "./actions/userAction";
+import { DarkModeToggle } from "@/components/ui/dark-mode-toggle";
 
 export default function Home() {
   const handleClick = async () => {
@@ -15,13 +16,9 @@ export default function Home() {
         Fetch
       </Button>
       <SignedIn>
-        {/* Mount the UserButton component */}
         <UserButton />
       </SignedIn>
-      {/* <Button>
-        Click Me
-      </Button> */}
-
+      <DarkModeToggle />
     </>
   );
 }
